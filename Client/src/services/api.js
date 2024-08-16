4087c787754d45e122f3362865c2edaaae1af5cc
Sompalkar <palkarsom1@gmail.com>
2024-08-16 23:18:28 +0530

    Hosted Backend

--- Client/src/services/api.js
+++ Client/src/services/api.js
@@ -5,7 +5,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://blogheaven.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
